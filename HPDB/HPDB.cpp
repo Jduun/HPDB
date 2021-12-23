d8102d73ad2df5c627acdde192f558533fc000f7
Станислав Исаев <isaevstanislav2003@mail.ru>
2021-12-23 11:32:29 +0300

    /

--- HPDB/HPDB.cpp
+++ HPDB/HPDB.cpp
@@ -747,7 +747,7 @@ bool compare(char* s1, char* s2)
         if (s1[i] > s2[i]) return true;
         else if (s1[i] != s2[i]) return false;
     }
-    return len1 > len2;
+    return len1 < len2;
 }
 
 bool is_letter(char c)
